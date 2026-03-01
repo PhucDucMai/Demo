442d7652c5dec0a8326adbdca187c5e496406edd
DucPhucMai <maiducphuc2001@gmail.com>
2026-03-01 15:24:35 +0700

    update from task-2

--- task-1.py
+++ task-1.py
@@ -1,1 +1,18 @@
+# Hello, this is comment from Mai Duc Phuc Task 2
+
 # Hello, this is comment from Mai Duc Phuc Task 1
+
+# Please don't tell to anyone about myself
+
+# What the fuck are you talking about?
+
+# Change from Task 2
+import pyjokes
+print(pyjokes.get_joke())
+
+print("Helicopter")
+
+if len(pyjokes.get_joke() % 2 == 0):
+    print("Shut the fuck off bitch")
+else:
+    print("Golden retrivers")
